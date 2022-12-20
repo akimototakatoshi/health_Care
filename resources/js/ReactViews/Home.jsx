e8f3e519fbc44ffa34dd49eb84bd8f0dfc8def51
keita miyajima <k.msli2218@gmail.com>
2022-12-20 18:08:18 +0900

    グラフページ、運動量の算出を追加しました。

--- resources/js/ReactViews/Home.jsx
+++ resources/js/ReactViews/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../modules/home.css";
-// import { useEffect, useState } from "react";
 import useSWR from "swr";
 import CalrieGraph from "./components/CalrieGraph";
+import Exercise from "./components/Exercise";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Home = () => {
@@ -12,33 +12,20 @@ const Home = () => {
     if (error) return <div>failed to load</div>;
     if (isLoading) return <div>loading...</div>;
 
-// console.log(data)
-    // let userdata = data[0]
-    // function aaa (){
-    //     if(userdata.gender === 0){
-    //     13.397 * userdata.weight + 4.799 * userdata.height - 5.677 * 25 + 88.362
-    //     }else if(userdata.gender === 1){
-    //         9.247 * userdata.weight + 3.098 * userdata.height - 4.33 * 25 + 447.593
-    //     }
-    // }
-
-    // console.log(aaa())
     return (
-        <div className="container">        
-            <div className="row">
-                <div className="col-3">
-                    <h3>必要運動量</h3>
-                    <ul>
-                        <li>ウォーキング：１０分</li>
-                        <li>ランニング：５分</li>
-                        <li>サイクリング：３分</li>
-                    </ul>
-                </div>
-                <div className="col-9">
-                    <CalrieGraph userData={data}/>
+        <div>
+            <div className="container">
+                <div className="card">
+                <div className="row">
+                    <div className=" col-auto">
+            <CalrieGraph userData={data} />
+                    </div>
+                    <div className="col mt-5 wf-roundedmplus1c">
+                        <Exercise userData={data} />
+                    </div>
+                    </div>
                 </div>
             </div>
-
             <div>
                 <ul className="nav-list row">
                     <li className="col-3">
--- resources/js/ReactViews/Index.jsx
+++ resources/js/ReactViews/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Src from "./Src";
 
-const root = ReactDOM.createRoot(document.getElementById("example"));
+const root = ReactDOM.createRoot(document.getElementById("example")) ;
 root.render(
     <React.StrictMode>
       <Src/>
